fdc188c8439a89b33e95e84962c8c996f4c10910
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2023-07-18 09:34:44 -0300

    refactor: memoize Vaadin version

--- src/main/java/com/flowingcode/vaadin/addons/demo/VaadinVersion.java
+++ src/main/java/com/flowingcode/vaadin/addons/demo/VaadinVersion.java
@@ -7,21 +7,25 @@ import org.slf4j.LoggerFactory;
 
 public class VaadinVersion {
 
+  private static String version;
+
   private VaadinVersion() {
     throw new UnsupportedOperationException();
   }
 
   public static String getVaadinVersion() {
-    try {
-      Properties pom = new Properties();
-      pom.load(VaadinVersion.class
-          .getResourceAsStream("/META-INF/maven/com.vaadin/vaadin-core/pom.properties"));
-      return Objects.requireNonNull((String) pom.get("version"));
-    } catch (Exception e) {
-      LoggerFactory.getLogger(Version.class.getName())
-          .warn("Unable to determine Vaadin version number", e);
-      return null;
+    if (version == null) {
+      try {
+        Properties pom = new Properties();
+        pom.load(VaadinVersion.class
+            .getResourceAsStream("/META-INF/maven/com.vaadin/vaadin-core/pom.properties"));
+        version = Objects.requireNonNull((String) pom.get("version"));
+      } catch (Exception e) {
+        LoggerFactory.getLogger(Version.class.getName())
+            .warn("Unable to determine Vaadin version number", e);
+      }
     }
+    return version;
   }
 
 }
