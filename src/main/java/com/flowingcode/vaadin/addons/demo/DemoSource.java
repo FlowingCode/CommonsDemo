798f02e4a2e212faa719b3dd200bb9cfbb06c9f4
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2023-08-19 12:06:05 -0300

    docs: correct annotation documentation

--- src/main/java/com/flowingcode/vaadin/addons/demo/DemoSource.java
+++ src/main/java/com/flowingcode/vaadin/addons/demo/DemoSource.java
@@ -29,7 +29,7 @@ import java.lang.annotation.Target;
 /**
  * This annotation is used for configuring the source code URL in a {@link TabbedDemo}. If no {@code
  * value} is specified, and the demo view is annotated with {@link GithubLink}, then the source URL
- * defaults to the location of the annotated class under {@code src/java/test} and the branch is
+ * defaults to the location of the annotated class under {@code src/test/java} and the branch is
  * determined from the value of {@link GithubBranch} in the demo view class (if the annotation is
  * present) or the containing package of the demo view class. If the source URL is defaulted and no
  * {@code GithubBranch} annotation is present either in the demo view class or its containing
