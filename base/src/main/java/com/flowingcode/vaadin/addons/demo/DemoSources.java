ea06ae9009717e59d5e4fa1a589423bdec6109b6
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2026-07-15 10:38:57 -0300

    feat: make DemoSource inheritable

--- base/src/main/java/com/flowingcode/vaadin/addons/demo/DemoSources.java
+++ base/src/main/java/com/flowingcode/vaadin/addons/demo/DemoSources.java
@@ -20,12 +20,14 @@
 package com.flowingcode.vaadin.addons.demo;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Inherited
 public @interface DemoSources {
 
   DemoSource[] value();
